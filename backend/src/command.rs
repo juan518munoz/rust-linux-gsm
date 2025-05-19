5f25ed3e1b9a698b809770571b712afda2cef56e
JM <juanmunoz890@gmail.com>
2025-05-19 09:23:33 -0300

    update commands

--- backend/src/command.rs
+++ backend/src/command.rs
@@ -1,4 +1,5 @@
 use std::{
+    env,
     path::PathBuf,
     process::{Command, Stdio},
 };
@@ -6,7 +7,8 @@ use std::{
 use crate::{GameServer, action::ServerAction};
 
 pub(crate) fn build_command(game_server: GameServer, action: ServerAction) -> Command {
-    let game_servers_dir = PathBuf::from("../game-servers");
+    // TODO: this path is only valid if running from the root of the project
+    let game_servers_dir = PathBuf::from(env::var("GAME_SERVER_DIR").unwrap_or(Default::default()));
 
     let mut cmd = Command::new("make");
     cmd.current_dir(game_servers_dir);
@@ -34,7 +36,7 @@ pub(crate) async fn run_command(mut command: Command) -> String {
     let mut child = match command.spawn() {
         Ok(child) => child,
         Err(e) => {
-            let error_message = format!("Failed to spawn command: {}", e);
+            let error_message = format!("Failed to spawn command '{:?}': {}", command, e);
             log::error!("{}", error_message);
             return error_message;
         }
@@ -43,7 +45,7 @@ pub(crate) async fn run_command(mut command: Command) -> String {
     match child.wait() {
         Ok(_) => "Command ran succesfully".to_string(),
         Err(e) => {
-            let error_message = format!("Failed to spawn command: {}", e);
+            let error_message = format!("Failed to run command '{:?}': {}", command, e);
             log::error!("{}", error_message);
             error_message
         }
